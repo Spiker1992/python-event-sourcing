309bc5ca81fc561e14f5e97cac0f4a69980ad82c
Vladislav Ogir <v.v.ogir@outlook.com>
2024-08-30 21:08:22 +0100

    test: added a test for the expected behaviour after post is created

---
+++ tests/test_project_posts.py
@@ -0,0 +1,21 @@
+import uuid
+from models import Post, PostTable
+from commons.event_store import EventStore
+from events import PostWasCreated
+
+
+def test_create_posts():
+    EventStore.reset_store()
+    stream_id = uuid.uuid4()
+    event = PostWasCreated(
+        title="FooBar", 
+        content="Lorem Ipsum"
+    )
+    EventStore.append(stream_id, event)
+
+    post = Post.objects.latest()
+    assert post.title == event.title  
+    assert post.content == event.content
+
+
+    
