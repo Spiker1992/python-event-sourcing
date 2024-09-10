82273923a9c32ff9b93ec9cb1341be57c34fca0b
Vlad <68771766+Spiker1992@users.noreply.github.com>
2024-09-10 23:18:30 +0100

    feat: subscribed post was published to a publish post projection

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
-from app.events import PostWasCreated
-from app.projection import CreatePost
+from app.events import PostWasCreated, PostWasPublished
+from app.projection import CreatePost, PublishPost
 from commons.event_bus import EventBus
 
 EventBus.subscribe(PostWasCreated, CreatePost)
+EventBus.subscribe(PostWasPublished, PublishPost)
