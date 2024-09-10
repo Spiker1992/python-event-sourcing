eed56f1ffd3b439417b2dafcd9782da6ea02764c
Vlad <68771766+Spiker1992@users.noreply.github.com>
2024-09-10 23:42:59 +0100

    feat: added subscribed for the confirmation email when post was published

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from app.events import PostWasCreated, PostWasPublished
 from app.projection import CreatePost, PublishPost
+from app.side_effects import ConfirmPostWasPublishedEmail
 from commons.event_bus import EventBus
 
 EventBus.subscribe(PostWasCreated, CreatePost)
 EventBus.subscribe(PostWasPublished, PublishPost)
+EventBus.subscribe(PostWasPublished, ConfirmPostWasPublishedEmail)
